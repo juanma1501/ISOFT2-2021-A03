6bf37a7b898f9ddb2971f9147e409608f20623ed
Juanma <juanmaporreroalmansa@gmail.com>
2020-12-20 17:39:45 +0100

    PR to master

--- RestaurantProjectISO/RestaurantProjectParent/src/main/java/com/uclm/iso2/RestaurantProjectParent/App.java
+++ RestaurantProjectISO/RestaurantProjectParent/src/main/java/com/uclm/iso2/RestaurantProjectParent/App.java
@@ -7,7 +7,7 @@ package com.uclm.iso2.RestaurantProjectParent;
 public class App 
 {
     public static void main( String[] args )
-    {
+    { 
         System.out.println( "Hello World!" );
     }
 }
